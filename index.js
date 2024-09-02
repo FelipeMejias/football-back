20992a1093f1d441566d414ffb96515c688ca998
Felipe Mejias <felipe.mejias50@gmail.com>
2024-09-02 03:43:25 -0300

    rodada3

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import cors from 'cors'
 import Express, {json} from 'express'
 import axios from 'axios'
 import { router } from './src/rotas.js'
+import { bla } from './teste2.js'
 const app=Express()
 app.use(cors())
 app.use(json())
@@ -19,49 +20,3 @@ async function buscar(){
         console.log(data)
 }
 
-
-
-function carro(){
-    const lista=[
-        'AIK','Brommapojkarna','Djurgarden','Elfsborg','GAIS','Gotemburgo','Hacken','Halmstad',
-        'Hammarby','Kalmar','Malmo','Mjallby','Norrkoping','Sirius','Varnamo','Vasteras'
-    ]
-    let string=''
-    for(let item of lista){
-        console.log(`import ${item} from './escudos/${item}.png'`)
-        string+=`${item},`
-    }
-    console.log(string)
-}
-function invert(){
-    const lista=[
-        ['brajuv','2406151830'],
-        ['fluago','2406152100'],
-        ['corsao','2406161600'],
-        ['vitint','2406161600'],
-        ['capfla','2406161600'],
-        ['vascru','2406161830'],
-        ['cribah','2406161830'],
-        ['grebot','2406161830'],
-        ['cuifor','2406161830'],
-        ['campal','2406172030'],
-       
-    ]
-    for(let item of lista.reverse()){
-        console.log(`['${item[0]}','${item[1]}'],`)
-    }
-}
-function onda(){
-    let str='['
-    for(let k=1;k<=191;k++){
-        let numero=k
-        if(k<10){
-            numero=`00${k}`
-        }else if(k<100){
-            numero=`0${k}`
-        }
-        str+=`onda${numero},`
-    }
-    str+=']'
-    console.log(str)
-}
--- teste2.js
+++ teste2.js
@@ -2,9 +2,14 @@ import { buildResultado } from "./src/profundo/resultado.js"
 
 export function bla(){
     const lista=[]
-    for(let k=70;k<=100;k+=5){
-        const {lucro}=buildResultado(['ing1','ita1','ale1','fra1','esp1'],['6'],k)
-        lista.push(parseFloat(lucro))
+    for(let k=1.8;k<=3.5;k+=0.1){
+        const {lucro,antigas}=buildResultado(['bra1'],['1','2','7','8'],90,k)
+        lista.push({k:k.toFixed(1),amostra:antigas.length,lucro:parseFloat(lucro)})
     }
-    console.log(lista)
+    const resp= lista.sort((a,b)=>{
+        if(a.lucro>b.lucro){
+            return -1
+        }else{return true}
+    })
+    console.log([resp[0],resp[1],resp[2]])
 }
